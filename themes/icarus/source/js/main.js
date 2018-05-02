1b17a014374688290cd62c64dec8005824a62ccb
kirito <250577914@qq.com>
2018-05-02 15:29:27 +0800

    更新主体

--- themes/icarus/source/js/main.js
+++ themes/icarus/source/js/main.js
@@ -1,21 +1,22 @@
 (function($){
-    var toTop = ($('#sidebar').height() - $(window).height()) + 60;
     // Caption
     $('.article-entry').each(function(i) {
-        $(this).find('img').each(function() {
-            if (this.alt) {
+        $(this).find('img').filter(function (element) {
+            return $(this).hasClass('');
+        }).each(function() {
+            // add image caption
+            if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
                 $(this).after('<span class="caption">' + this.alt + '</span>');
             }
 
-            // 对于已经包含在链接内的图片不适用lightGallery
             if ($(this).parent().prop("tagName") !== 'A') {
-                $(this).wrap('<a href="' + this.src + '" title="' + this.alt + '" class="gallery-item"></a>');
+                $(this).wrap('<a href="' + ($(this).attr("data-imgbig") ? $(this).attr("data-imgbig") : this.src) + '" title="' + this.alt + '" class="gallery-item"></a>');
             }
         });
     });
-    if (lightGallery) {
+    if (typeof lightGallery != 'undefined') {
         var options = {
-            selector: '.gallery-item',
+            selector: '.gallery-item'
         };
         $('.article-entry').each(function(i, entry) {
             lightGallery(entry, options);
@@ -32,32 +33,94 @@
     }
 
     // Profile card
+    var profileElem = $('#profile');
     $(document).on('click', function () {
-        $('#profile').removeClass('card');
+        profileElem.removeClass('card');
     }).on('click', '#profile-anchor', function (e) {
         e.stopPropagation();
-        $('#profile').toggleClass('card');
+        profileElem.toggleClass('card');
     }).on('click', '.profile-inner', function (e) {
         e.stopPropagation();
     });
 
     // To Top
-    if ($('#sidebar').length) {
+    var sidebarElem = $('#sidebar'),
+        toTopElem = $('#toTop');
+
+    (function () {
+        if (!sidebarElem.length) return;
+
+        checkDisplayToTop();
         $(document).on('scroll', function () {
-            if ($(document).width() >= 800) {
-                if(($(this).scrollTop() > toTop) && ($(this).scrollTop() > 0)) {
-                    $('#toTop').fadeIn();
-                    $('#toTop').css('left', $('#sidebar').offset().left);
-                } else {
-                    $('#toTop').fadeOut();
-                }
-            } else {
-                $('#toTop').fadeIn();
-                $('#toTop').css('right', 20);
-            }
-        }).on('click', '#toTop', function () {
+            checkDisplayToTop();
+        });
+
+        toTopElem.click(function () {
             $('body, html').animate({ scrollTop: 0 }, 600);
         });
-    }
+
+        var isToTopDisplayed = false;
+        function checkDisplayToTop() {
+            var toTop = (sidebarElem.height() - $(window).height()) + 60;
+            var currentScrollTop = $(document).scrollTop();
+            var needDisplay = (currentScrollTop > toTop) && (currentScrollTop > 0);
+
+            if ($(document).width() >= 800) {
+                if (needDisplay) {
+                    if (isToTopDisplayed) return;
+                    toTopElem.fadeIn();
+                    toTopElem.css('left', sidebarElem.offset().left);
+                    isToTopDisplayed = true;
+                } else {
+                    if (!isToTopDisplayed) return;
+                    toTopElem.fadeOut();
+                    isToTopDisplayed = false;
+                }
+            } else {
+                toTopElem.show();
+                toTopElem.css('right', 20);
+            }
+        }
+    })();
+
+    // Fixed Profile
+    (function () {
+        checkFixedProfile();
+        $(document).on('scroll', function () {
+            checkFixedProfile();
+        });
+
+        var isFixedProfile = false;
+        function checkFixedProfile() {
+            if (!profileElem.is('.profile-fixed')) return;
+            if ($(document).width() < 800) return;
+
+            var currentScrollTop = $(document).scrollTop();
+            var profileInnerElem = $('#profile .profile-inner');
+            var needFixed = currentScrollTop >= profileElem.offset().top + profileElem.outerHeight(true);
+
+            if (needFixed) {
+                if (isFixedProfile) return;
+
+                profileInnerElem.css('position', 'fixed')
+                    .css('width', profileElem.innerWidth() + 'px')
+                    .css('top', '0');
+
+                // css animation fade-in
+                profileInnerElem.css('animation', '');
+                profileInnerElem.addClass('anim-fade-in');
+                isFixedProfile = true;
+            } else {
+                if (!isFixedProfile) return;
+
+                profileInnerElem.css('position', '')
+                    .css('width', '')
+                    .css('top', '');
+
+                profileInnerElem.css('animation', 'none');
+                isFixedProfile = false;
+            }
+        }
+    })();
 
 })(jQuery);
