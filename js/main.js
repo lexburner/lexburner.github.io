b4611e5cc82e8ffdda0638700256f14605fe3c43
xujingfeng <jingfeng.xjf@alibaba-inc.com>
2021-04-09 02:30:12 +0800

    Site updated: 2021-04-09 02:30:12

--- js/main.js
+++ js/main.js
@@ -20,12 +20,6 @@
         $('.justified-gallery').justifiedGallery();
     }
 
-    // if (typeof moment === 'function') {
-    //     $('.article-meta time').each(function() {
-    //         $(this).text(moment($(this).attr('datetime')).fromNow());
-    //     });
-    // }
-
     $('.article > .content > table').each(function() {
         if ($(this).width() > $(this).parent().width()) {
             $(this).wrap('<div class="table-overflow"></div>');
